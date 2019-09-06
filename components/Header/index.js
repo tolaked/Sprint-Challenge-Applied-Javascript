9cb8e797501d5cb760f013350bdca233f3a9b142
Akere Adetola <adetolaakere1@gmail.com>
2019-09-06 11:03:33 +0100

    initial commit

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+  let topDiv = document.createElement("div");
+  topDiv.classList.add("header");
+
+  let span = document.createElement("span");
+  span.classList.add("date");
+  span.textContent = "SMARCH 28,2019";
+
+  let heading = document.createElement("h1");
+  heading.textContent = "Lambda Times";
+
+  let span2 = document.createElement("span");
+  span2.classList.add("temp");
+  span2.textContent = "98°";
+
+  let allElements = [span, heading, span2];
+  allElements.map(element => topDiv.appendChild(element));
+
+  return topDiv;
+}
+
+const container = document.querySelector(".header-container");
+let card = Header();
+container.appendChild(card);
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,30 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const bigContainer = document.querySelector(".header-container");
+
+axios
+  .get(`https://lambda-times-backend.herokuapp.com/topics`)
+  .then(response => {
+    // Remember response is an object, response.data is an array.
+    let topicsTabs = response.data.topics;
+
+    let newTabs = allTbas();
+    topicsTabs.forEach(topic => {
+      let h1 = document.createElement("h1");
+      h1.textContent = topic;
+      newTabs.appendChild(h1);
+    });
+    bigContainer.appendChild(newTabs);
+  })
+  .catch(error => {
+    console.log("Error:", error);
+  });
+
+function allTbas() {
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("tab");
+
+  return newDiv;
+}
